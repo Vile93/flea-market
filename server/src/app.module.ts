8c2b0ae698498cb31ab120edfe293e5c8d72297c
Vile <vilemusicyt@gmail.com>
2025-04-15 13:57:16 +0300

    (server) added .env configuration

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [PrismaModule, UserModule],
+    imports: [PrismaModule, UserModule, ConfigModule.forRoot({ envFilePath: '../.env' })],
     controllers: [AppController],
     providers: [AppService, PrismaService],
 })
