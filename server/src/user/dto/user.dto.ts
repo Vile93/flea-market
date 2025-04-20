78384b08956f3b1db814f04a5693cc27947f87ba
Vile <vilemusicyt@gmail.com>
2025-04-20 09:54:46 +0300

    (server) added dto

---
+++ server/src/user/dto/user.dto.ts
@@ -0,0 +1,20 @@
+import { Role } from '@prisma/client';
+import { Exclude } from 'class-transformer';
+
+export class UserDto {
+    constructor(partial: Partial<UserDto>) {
+        Object.assign(this, partial);
+    }
+
+    id: number;
+    name: string;
+    surname: string;
+    username: string;
+    email: string;
+    phone: string;
+    is_verified: boolean;
+    avatar_path: string;
+    role: Role;
+    @Exclude()
+    password: string;
+}
