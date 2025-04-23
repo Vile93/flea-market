856b397c2669476265dc621d214a390a16ca84dd
Vile <vilespeedyt@gmail.com>
2025-04-23 14:15:50 -0700

    🚀 feat(server): add AWS S3 SDK for storage operations
    - add new dependencies for AWS S3 client and request presigner
    - define new environment variables for S3 bucket region and name
    
    🔧 chore(server): update storage module for AWS S3 integration
    
    - remove nestjs-s3 import and configuration
    - create S3Client instance with necessary credentials and region
    - implement file upload, delete, and get URL methods using AWS S3 SDK

--- server/src/storage/storage.service.ts
+++ server/src/storage/storage.service.ts
@@ -1,7 +1,65 @@
+import { S3Client, ObjectCannedACL, PutObjectCommand, DeleteObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { Injectable } from '@nestjs/common';
-import { InjectS3, S3 } from 'nestjs-s3';
+import { ConfigService } from '@nestjs/config';
+import { randomUUID } from 'crypto';
+import path from 'path';
+import { ENV } from 'src/common/types/env.enum';
 
 @Injectable()
 export class StorageService {
-    constructor(@InjectS3() private readonly storage: S3) {}
+    private client: S3Client;
+    constructor(private readonly configService: ConfigService) {
+        this.client = new S3Client({
+            region: configService.getOrThrow(ENV.BUCKET_REGION),
+            endpoint: configService.getOrThrow(ENV.BUCKET_URL),
+            credentials: {
+                accessKeyId: configService.getOrThrow(ENV.BUCKET_ACCESS_KEY),
+                secretAccessKey: configService.getOrThrow(ENV.BUCKET_SECRET_KEY),
+            },
+        });
+    }
+
+    async uploadFile(file: Express.Multer.File, ACL: ObjectCannedACL) {
+        const uuid = randomUUID();
+        const fileName = uuid + path.extname(file.originalname);
+        const command = new PutObjectCommand({
+            Bucket: this.configService.getOrThrow(ENV.BUCKET_NAME),
+            Key: fileName,
+            Body: file.buffer,
+            ACL,
+        });
+        try {
+            await this.client.send(command);
+            return fileName;
+        } catch {
+            return null;
+        }
+    }
+
+    async deleteFile(path: string): Promise<boolean> {
+        const command = new DeleteObjectCommand({
+            Bucket: this.configService.getOrThrow(ENV.BUCKET_NAME),
+            Key: path,
+        });
+        try {
+            await this.client.send(command);
+            return true;
+        } catch {
+            return false;
+        }
+    }
+
+    async getUrlFile(path: string) {
+        const command = new GetObjectCommand({
+            Bucket: this.configService.getOrThrow(ENV.BUCKET_NAME),
+            Key: path,
+        });
+        try {
+            const url = await getSignedUrl(this.client, command);
+            return url;
+        } catch {
+            return null;
+        }
+    }
 }
