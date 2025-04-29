bb126b3c1551afb9a5264c44afe7dd01af5dd435
Vile <vilespeedyt@gmail.com>
2025-04-29 14:33:42 -0700

    📝 docs(client): add configuration for new components
    - create components.json file with initial component configuration including styles, aliases, and icon library
    
    🔧 chore(client): update eslint and next config
    
    - update eslint configuration to use single quotes consistently
    - add environment variable configuration for backend API in next.config.ts
    
    📦 build(client): update package dependencies
    
    - add new dependencies for form handling, UI components, and utilities
    - update existing dependencies for optimization and bug fixes
    
    📝 docs(client): update postcss configuration
    
    - adjust indentation and quotes for plugins in postcss.config.mjs
    
    📝 docs(client): add global styles for dark mode
    
    - define custom variant for dark mode styles
    - add color variables and styles for dark mode in globals.css
    
    📝 docs(client): create CSS modules for component layout
    
    - create style.module.css file to define grid layout for filter, search, sort, and offers components

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    env: {
+        BACKEND_API: process.env.BACKEND_API,
+    },
 };
 
 export default nextConfig;
