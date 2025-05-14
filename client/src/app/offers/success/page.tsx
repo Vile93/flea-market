bd9d419e40714933a2d911952c98aa9c1a840b8e
Vile <vilespeedyt@gmail.com>
2025-05-14 15:55:25 -0700

    ✨ feat(client): add success page components
    - create SuccessForm component for success page
    - implement SuccessPage component rendering SuccessForm
    - introduce metadata object for title and description in Home page

---
+++ client/src/app/offers/success/page.tsx
@@ -0,0 +1,5 @@
+import { SuccessForm } from '@/app/offers/success/_compoenents/success-form';
+
+export default function SuccessPage() {
+    return <SuccessForm />;
+}
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,11 @@
+import { Metadata } from 'next';
 import { Filter } from './_components/filter';
 import { getOfferCategoriesAndLocations } from '@/api/server.api';
+
+export const metadata: Metadata = {
+    title: 'Zorka',
+    description: 'Zorka',
+};
 
 export default async function Home() {
     const data = await getOfferCategoriesAndLocations();
